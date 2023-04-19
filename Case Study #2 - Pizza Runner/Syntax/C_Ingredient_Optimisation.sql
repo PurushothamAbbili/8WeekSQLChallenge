b3ee8a58e5f9cfee05b96aed82bf519b741ae8c2
Purushotham Abbili <84988851+PurushothamAbbili@users.noreply.github.com>
2023-04-19 08:11:56 +0000

    Update

--- Case Study #2 - Pizza Runner/Syntax/C_Ingredient_Optimisation.sql
+++ Case Study #2 - Pizza Runner/Syntax/C_Ingredient_Optimisation.sql
@@ -2,3 +2,178 @@
 -- C. INGREDIENT OPTIMISATION --
 --------------------------------
 
+-- 1. What are the standard ingredients for each pizza?
+
+SELECT 
+    p.pizza_name,
+    GROUP_CONCAT(t.topping_name
+        SEPARATOR ', ') AS ingredients
+FROM
+    toppingsBreak t
+        JOIN
+    pizza_names p ON t.pizza_id = p.pizza_id
+GROUP BY p.pizza_name;
+
+
+-- 2. What was the most commonly added extra?
+
+SELECT 
+    p.topping_name, COUNT(*) AS extra_count
+FROM
+    extrasBreak e
+        JOIN
+    pizza_toppings p ON e.extra_id = p.topping_id
+GROUP BY p.topping_name
+ORDER BY COUNT(*) DESC;
+
+
+-- 3. What was the most common exclusion?
+
+SELECT 
+    p.topping_name, COUNT(*) AS exclusion_count
+FROM
+    exclusionsBreak e
+        JOIN
+    pizza_toppings p ON e.exclusion_id = p.topping_id
+GROUP BY p.topping_name
+ORDER BY COUNT(*) DESC;
+
+
+-- 4. Generate an order item for each record in the customers_orders table in the format of one of the following:
+-- Meat Lovers
+-- Meat Lovers - Exclude Beef
+-- Meat Lovers - Extra Bacon
+-- Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
+
+WITH cteExtras AS (
+  SELECT 
+    e.record_id,
+    CONCAT('Extra ', GROUP_CONCAT(t.topping_name SEPARATOR ', ')) AS record_options
+  FROM extrasBreak e
+  JOIN pizza_toppings t
+    ON e.extra_id = t.topping_id
+  GROUP BY e.record_id
+), 
+cteExclusions AS (
+  SELECT 
+    e.record_id,
+    CONCAT('Exclusion ', GROUP_CONCAT(t.topping_name SEPARATOR ', ')) AS record_options
+  FROM exclusionsBreak e
+  JOIN pizza_toppings t
+    ON e.exclusion_id = t.topping_id
+  GROUP BY e.record_id
+), 
+cteUnion AS (
+  SELECT * FROM cteExtras
+  UNION ALL
+  SELECT * FROM cteExclusions
+)
+
+SELECT 
+  c.record_id,
+  c.order_id,
+  c.customer_id,
+  c.pizza_id,
+  c.order_time,
+  CONCAT_WS(' - ', p.pizza_name, GROUP_CONCAT(u.record_options SEPARATOR ' - ')) AS pizza_info
+FROM customer_orders_temp c
+LEFT JOIN cteUnion u
+  ON c.record_id = u.record_id
+JOIN pizza_names p
+  ON c.pizza_id = p.pizza_id
+GROUP BY
+  c.record_id, 
+  c.order_id,
+  c.customer_id,
+  c.pizza_id,
+  c.order_time,
+  p.pizza_name
+ORDER BY record_id;
+
+
+-- 5. Generate an alphabetically ordered comma separated ingredient list for each pizza order 
+-- from the customer_orders table and add a 2x in front of any relevant ingredients
+-- For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
+
+-- Create a CTE: Each line displays an ingredient for an ordered pizza (add 2x for extras and remove exclusions as well)
+WITH ingredients AS (
+  SELECT 
+    c.*,
+    p.pizza_name,
+
+    -- Add a 2x in front of topping_names if their topping_id appear in the extrasBreak table
+    CASE WHEN t.topping_id IN (
+          SELECT extra_id 
+          FROM extrasBreak e 
+          WHERE e.record_id = c.record_id)
+      THEN CONCAT('2x', t.topping_name)
+      ELSE t.topping_name
+    END AS topping
+
+  FROM customer_orders_temp c
+  JOIN toppingsBreak t
+    ON t.pizza_id = c.pizza_id
+  JOIN pizza_names p
+    ON p.pizza_id = c.pizza_id
+
+  -- Exclude toppings if their topping_id appear in the exclusionBreak table
+  WHERE t.topping_id NOT IN (
+      SELECT exclusion_id 
+      FROM exclusionsBreak e 
+      WHERE c.record_id = e.record_id)
+)
+
+SELECT 
+  record_id,
+  order_id,
+  customer_id,
+  pizza_id,
+  order_time,
+  CONCAT(pizza_name, ': ', GROUP_CONCAT(topping SEPARATOR ', ')) AS ingredients_list
+FROM ingredients
+GROUP BY 
+  record_id, 
+  record_id,
+  order_id,
+  customer_id,
+  pizza_id,
+  order_time,
+  pizza_name
+ORDER BY record_id;
+
+
+-- 6. What is the total quantity of each ingredient used in all delivered pizzas sorted by most frequent first?
+
+WITH frequentIngredients AS (
+  SELECT 
+    c.record_id,
+    t.topping_name,
+    CASE
+      -- if extra ingredient, add 2
+      WHEN t.topping_id IN (
+          SELECT e.extra_id 
+          FROM extrasBreak e
+          WHERE e.record_id = c.record_id) 
+      THEN 2
+      -- if excluded ingredient, add 0
+      WHEN t.topping_id IN (
+          SELECT e.exclusion_id 
+          FROM exclusionsBreak e 
+          WHERE c.record_id = e.record_id)
+      THEN 0
+      -- no extras, no exclusions, add 1
+      ELSE 1
+    END AS times_used
+  FROM customer_orders_temp c
+  JOIN toppingsBreak t
+    ON t.pizza_id = c.pizza_id
+  JOIN pizza_names p
+    ON p.pizza_id = c.pizza_id
+)
+
+SELECT 
+  topping_name,
+  SUM(times_used) AS times_used 
+FROM frequentIngredients
+GROUP BY topping_name
+ORDER BY times_used DESC;
